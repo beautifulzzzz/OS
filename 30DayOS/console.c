556d4a9665261b78249bd275b790a89ae1bd752a
litao <953521476@qq.com>
2015-01-26 22:40:07 +0800

    change

--- 30DayOS/console.c
+++ 30DayOS/console.c
@@ -1,4 +1,4 @@
-
+﻿
 #include "bootpack.h"
 #include <string.h>
 #include <stdio.h>
@@ -18,6 +18,7 @@ void console_task(struct SHEET *sheet, unsigned int memtotal)
 	cons.cur_x =  8;
 	cons.cur_y = 28;
 	cons.cur_c = -1;
+	*((int*)0x0fec)=(int)&cons;
 	
 	fifo32_init(&task->fifo, 128, fifobuf, task);
 	timer = timer_alloc();//分配时钟
@@ -152,6 +153,24 @@ void cons_newline(struct CONSOLE *cons)
 	return;
 }
 /////////////////////////////////////////////////////////////////////////////////////
+//功能：两种方式显示字符串
+//参数：
+void cons_putstr0(struct CONSOLE *cons, char *s)
+{
+	for (; *s != 0; s++) {
+		cons_putchar(cons, *s, 1);
+	}
+	return;
+}
+void cons_putstr1(struct CONSOLE *cons, char *s, int l)
+{
+	int i;
+	for (i = 0; i < l; i++) {
+		cons_putchar(cons, s[i], 1);
+	}
+	return;
+}
+/////////////////////////////////////////////////////////////////////////////////////
 //功能：运行窗口命令
 //参数：
 void cons_runcmd(char *cmdline, struct CONSOLE *cons, int *fat, unsigned int memtotal)
@@ -164,13 +183,13 @@ void cons_runcmd(char *cmdline, struct CONSOLE *cons, int *fat, unsigned int mem
 		cmd_dir(cons);
 	} else if (strncmp(cmdline, "type ", 5) == 0) {
 		cmd_type(cons, fat, cmdline);
-	} else if (strcmp(cmdline, "hlt") == 0) {
-		cmd_hlt(cons, fat);
+//	} else if (strcmp(cmdline, "hlt") == 0) {
+//		cmd_hlt(cons, fat);
 	} else if (cmdline[0] != 0) {
-		/* 不是命令也不是空行 */
-		putfonts8_asc_sht(cons->sht, 8, cons->cur_y, COL8_FFFFFF, COL8_000000, "Bad command.", 12);
-		cons_newline(cons);
-		cons_newline(cons);
+		if(cmd_app(cons,fat,cmdline)==0){
+			/* 不是命令也不是空行 */
+			cons_putstr0(cons, "Bad command.\n\n");
+		}
 	}
 	return;
 }
@@ -180,14 +199,9 @@ void cons_runcmd(char *cmdline, struct CONSOLE *cons, int *fat, unsigned int mem
 void cmd_mem(struct CONSOLE *cons, unsigned int memtotal)
 {
 	struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
-	char s[30];
-	sprintf(s, "total   %dMB", memtotal / (1024 * 1024));
-	putfonts8_asc_sht(cons->sht, 8, cons->cur_y, COL8_FFFFFF, COL8_000000, s, 30);
-	cons_newline(cons);
-	sprintf(s, "free %dKB", memman_total(memman) / 1024);
-	putfonts8_asc_sht(cons->sht, 8, cons->cur_y, COL8_FFFFFF, COL8_000000, s, 30);
-	cons_newline(cons);
-	cons_newline(cons);
+	char s[60];
+	sprintf(s, "total   %dMB\nfree %dKB\n\n", memtotal / (1024 * 1024), memman_total(memman) / 1024);
+	cons_putstr0(cons, s);
 	return;
 }
 /////////////////////////////////////////////////////////////////////////////////////
@@ -227,7 +241,7 @@ void cmd_dir(struct CONSOLE *cons)
 				s[ 9] = finfo[i].ext[0];
 				s[10] = finfo[i].ext[1];
 				s[11] = finfo[i].ext[2];
-				putfonts8_asc_sht(cons->sht, 8, cons->cur_y, COL8_FFFFFF, COL8_000000, s, 30);
+				cons_putstr0(cons, s);
 				cons_newline(cons);
 			}
 		}
@@ -248,19 +262,16 @@ void cmd_type(struct CONSOLE *cons, int *fat, char *cmdline)
 		/* 找到文件的情况 */
 		p = (char *) memman_alloc_4k(memman, finfo->size);//分配一个和文件大小一样的内存
 		file_loadfile(finfo->clustno, finfo->size, p, fat, (char *) (ADR_DISKIMG + 0x003e00));//将文件的内容读入内存，这样文件的内容已经排列为正确的顺序
-		for (i = 0; i < finfo->size; i++) {//输出内容，掉用cons_putchar函数
-			cons_putchar(cons, p[i], 1);
-		}
+		cons_putstr1(cons, p, finfo->size);//输出内容
 		memman_free_4k(memman, (int) p, finfo->size);//释放内存
 	} else {
 		/* 没有找到文件的情况 */
-		putfonts8_asc_sht(cons->sht, 8, cons->cur_y, COL8_FFFFFF, COL8_000000, "File not found.", 15);
-		cons_newline(cons);
+		cons_putstr0(cons, "File not found.\n");
 	}
 	cons_newline(cons);//新行
 	return;
 }
-/////////////////////////////////////////////////////////////////////////////////////
+/*////////////////////////////////////////////////////////////////////////////////////
 //功能：hlt命令,运行hlt程序
 //参数：
 void cmd_hlt(struct CONSOLE *cons, int *fat)
@@ -270,17 +281,91 @@ void cmd_hlt(struct CONSOLE *cons, int *fat)
 	struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *) ADR_GDT;
 	char *p;
 	if (finfo != 0) {
-		/* 有文件 */
+		//有文件
 		p = (char *) memman_alloc_4k(memman, finfo->size);//申请一个内存地址大小一样的p
 		file_loadfile(finfo->clustno, finfo->size, p, fat, (char *) (ADR_DISKIMG + 0x003e00));//读取FAT到p
 		set_segmdesc(gdt + 1003, finfo->size - 1, (int) p, AR_CODE32_ER);//将hlt.hrb读入内存后将其注册为GDT1003号，因为1~2号右dsctbl.c用，而3~1002号由mtask.c
-		farjmp(0, 1003 * 8);//然后调用跳转运行
+		farcall(0,1003 * 8);//转到
 		memman_free_4k(memman, (int) p, finfo->size);//释放内存
 	} else {
-		/* 无文件 */
+		//无文件 
 		putfonts8_asc_sht(cons->sht, 8, cons->cur_y, COL8_FFFFFF, COL8_000000, "File not found.", 15);
 		cons_newline(cons);
 	}
 	cons_newline(cons);
 	return;
+}*/
+/////////////////////////////////////////////////////////////////////////////////////
+//功能：运行程序命令
+//参数：只要输入文件名就能执行该文件，应用程序
+int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline)
+{
+	struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
+	struct FILEINFO *finfo;
+	struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *) ADR_GDT;
+	char name[18], *p;
+	int i;
+
+	//根据命令行生成文件名
+	for (i = 0; i < 13; i++) {
+		if (cmdline[i] <= ' ') {
+			break;
+		}
+		name[i] = cmdline[i];
+	}
+	name[i] = 0; //暂且将文件名后面置为0
+
+	//寻找文件
+	finfo = file_search(name, (struct FILEINFO *) (ADR_DISKIMG + 0x002600), 224);
+	if (finfo == 0 && name[i - 1] != '.') {
+		//由于找不到文件，故在文件名后面加上.HRB后重新寻找
+		name[i    ] = '.';
+		name[i + 1] = 'H';
+		name[i + 2] = 'R';
+		name[i + 3] = 'B';
+		name[i + 4] = 0;
+		finfo = file_search(name, (struct FILEINFO *) (ADR_DISKIMG + 0x002600), 224);
+	}
+
+	if (finfo != 0) {
+		//找到文件
+		p = (char *) memman_alloc_4k(memman, finfo->size);
+		*((int*)0xfe8)=(int)p;//把代码段的起始地址存入内存，供显示字符串时寻找字符串
+		file_loadfile(finfo->clustno, finfo->size, p, fat, (char *) (ADR_DISKIMG + 0x003e00));//读取FAT到p
+		set_segmdesc(gdt + 1003, finfo->size - 1, (int) p, AR_CODE32_ER);//将.hrb读入内存后将其注册为GDT1003号，因为1~2号右dsctbl.c用，而3~1002号由mtask.c
+		if (finfo->size >= 8 && strncmp(p + 4, "Hari", 4) == 0) {//把开头的六字节替换为
+			p[0] = 0xe8;
+			p[1] = 0x16;
+			p[2] = 0x00;
+			p[3] = 0x00;
+			p[4] = 0x00;
+			p[5] = 0xcb;
+		}
+		farcall(0, 1003 * 8);//转到
+		memman_free_4k(memman, (int) p, finfo->size);//内存释放
+		cons_newline(cons);
+		return 1;
+	}
+	//没有找到，就返回0
+	return 0;
 }
+/////////////////////////////////////////////////////////////////////////////////////
+//功能：API处理程序
+//参数：
+//功能号1————————显示单个字符(AL=字符编码)
+//功能号2————————显示字符串0(EBX=字符串地址)
+//功能号3————————显示字符串1(EBX=字符串地址,ECX=字符串长度)
+void hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int eax)
+{
+	int cs_base=*((int*)0xfe8);//获取保存在内存中的代码段的起始地址
+	struct CONSOLE *cons = (struct CONSOLE *) *((int *) 0x0fec);
+	if (edx == 1) {
+		cons_putchar(cons, eax & 0xff, 1);
+	} else if (edx == 2) {
+		cons_putstr0(cons, (char *) ebx+cs_base);
+	} else if (edx == 3) {
+		cons_putstr1(cons, (char *) ebx+cs_base, ecx);
+	}
+	return;
+}
+
